9574306fd5b53cf4d6bfb4d32152e98a25babc69
skku-sungjin <skku7714@gmail.com>
2020-09-19 03:16:47 +0900

    react-bootstrap for table, divide table mother and child

--- src/components/Fetch.js
+++ src/components/Fetch.js
@@ -3,22 +3,17 @@ import allActions from "../actions";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import PropTypes from "prop-types";
+import TableMother from "./TableMother";
 
+/*
+ * fetch all data and store to redux
+ * pass query data to TableMother.js
+ */
 const Fetch = ({ load, loading, data }) => {
   const dispatch = useDispatch();
   const search = useSelector((state) => state.form.search);
   let regex = new RegExp(search, "gi");
   const [test, setTest] = useState();
-
-  useEffect(() => {
-    console.log("useEffect start");
-    console.log(search.values.search);
-    console.log(regex);
-    console.log("use effect in fetch");
-
-    regex = new RegExp(search.values.search, "gi");
-    setTest(regex);
-  }, [search]);
 
   useEffect(() => {
     console.log("GET ALL DATA START");
@@ -28,28 +23,10 @@ const Fetch = ({ load, loading, data }) => {
 
   return (
     <>
-      <h1>Fetch Data</h1>
-      <h3>
-        load state: {load && "Now Loaded"}
-        {loading && "Now Loading"}
-      </h3>
-      <h1>====== RESULT DATA ======</h1>
+      load state: {load && "Now Loaded"}
+      {loading && "Now Loading"}
       <div>
-        {data.map((v, idx) => {
-          if (
-            v.name.match(test) ||
-            v.alpha2Code.match(test) ||
-            v.capital.match(test) ||
-            v.region.match(test)
-          ) {
-            return <div key={idx}>{v.name}</div>;
-          } else {
-            const callingCodes = v.callingCodes.filter((v) => v.match(test));
-            if (callingCodes.length) {
-              return <div key={idx}>{v.name}</div>;
-            }
-          }
-        })}
+        <TableMother search={search} regex={regex} />
       </div>
     </>
   );
