0b26596ea6293418d0d397f72e742de9122e9b9d
skku-sungjin <skku7714@gmail.com>
2020-09-19 15:01:31 +0900

    table sorting for each column

--- src/actions/QueryAction.js
+++ src/actions/QueryAction.js
@@ -1,7 +1,11 @@
 const queryAction = (payload) => {
   return { type: "QUERY_BY_SEARCH", payload: payload };
 };
+const querySort = ({ query, sortBy, order }) => {
+  return { type: "QUERY_BY_SORT", payload: { query, sortBy, order } };
+};
 
 export default {
   queryAction,
+  querySort,
 };
